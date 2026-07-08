56ced95466cd3461823bb67d69be5bf773262ea2
Mehtab <mehtab185198131@gmail.com>
2026-07-08 13:09:42 +0530

    Create mathOperations.js

---
+++ 01-node-revision/01-import_export_module/01-commonjs_method/mathOperations.js
@@ -0,0 +1,16 @@
+
+// addition 
+function add(a, b) {return a + b};
+
+// multiplication 
+function sub(a, b) {return a - b};
+
+
+const developerName = 'Mehtab';
+
+
+module.exports = {
+    addition :add, 
+    subtraction  : sub,
+    developer : developerName
+}
