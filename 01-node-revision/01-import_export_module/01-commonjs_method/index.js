7343b38475ad79b23f368b07ebcf312b20873647
Mehtab <mehtab185198131@gmail.com>
2026-07-08 13:09:14 +0530

    Implement CommonJS module import and usage

---
+++ 01-node-revision/01-import_export_module/01-commonjs_method/index.js
@@ -0,0 +1,8 @@
+
+const math  = require('./mathOperations');
+
+console.log("=== CommonJS (require) Practice ===");
+
+console.log('Addition :', math.addition(8,12));
+console.log('Subtraction :', math.subtraction(12,6));
+console.log('Developer :', math.developer);
