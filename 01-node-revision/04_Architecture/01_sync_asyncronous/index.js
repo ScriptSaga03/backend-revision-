2462066529d3c7ccd1dab1dbce72c30f6b288c87
Mehtab <mehtab185198131@gmail.com>
2026-07-18 23:55:06 +0530

    Add example of synchronous and asynchronous code
    Demonstrate blocking and non-blocking behavior in Node.js.

---
+++ 01-node-revision/04_Architecture/01_sync_asyncronous/index.js
@@ -0,0 +1,24 @@
+
+// blocking or Sync
+// console.log("Starting the application...");
+
+
+// for (let i = 0 ; i <  i < 1000000000; i++) {};
+
+// console.log("closed the application!")
+
+
+// non-blocking or async
+
+console.log('1: order 1')
+setTimeout(()=>{
+    console.log("2: order 1 is ready to served.")
+},3000);
+
+
+console.log('3: order 2');
+
+
+
+
+
